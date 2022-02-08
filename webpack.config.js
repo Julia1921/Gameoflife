56715a788b75ee9d43597246f94a0bacd991d9c7
Юля Круглова <juliakruglovka@gmail.com>
2022-02-08 17:09:09 +0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+ module.exports = {
+     mode: 'development',
+     entry: {
+         main: path.resolve(__dirname, './src/index.ts'),
+     },
+     devtool: 'source-map',
+     output: {
+         path: path.resolve(__dirname, './dist'),
+         filename: '[name].bindle.js',
+     },
+     devServer: {
+        static: {
+            directory: path.join(__dirname, './dist'),
+          },
+        open: true,
+        compress: true,
+        hot: true,
+        port: 9000,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+     module: {
+         rules: [
+             {
+                 test: /\.(scss|.css)$/,
+                 use: ['style-loader', 'css-loader', 'sass-loader']
+             },
+             {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+         ]
+     }, 
+     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin()
+    ],  
+ }
